db38ba0027978bc02854b9a9bbd2c8b61ee85bd4
zhangyang.521 <zhangyang.521@bytedance.com>
2022-01-14 11:57:54 +0800

    goroutine实现fib
    Change-Id: Ib815475a8e3ba6c64b36bcc913e4d89483f0b297

---
+++ MyGoroutine/Goroutine.go
@@ -0,0 +1,31 @@
+package MyGoroutine
+
+import (
+	"fmt"
+	"time"
+)
+
+// 注意需要导出的函数首字母要大写
+
+func Myfib() {
+	go spinner(100 * time.Millisecond)
+	const n = 45
+	fibN := fib(n)
+	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+}
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+func fib(x int) int {
+	if x < 2 {
+		return x
+	}
+	return fib(x-1) + fib(x-2)
+}
